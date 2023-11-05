073a7bd8df52017e9b38955a6a1f2fb52967b8b9
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-11-06 00:08:45 +0300

    [+] mark's disabled

--- tests/transition_my_account/test_dropdown.py
+++ tests/transition_my_account/test_dropdown.py
@@ -4,8 +4,8 @@ from time import sleep
 import pytest
 
 
-@pytest.mark.skip
-@pytest.mark.xfail
+# @pytest.mark.skip
+# @pytest.mark.xfail
 def test_my_account(driver):
     page = LoginPage(driver)
     page.sign_in(page)
@@ -16,21 +16,21 @@ def test_my_account(driver):
     assert page.current_url == 'https://magento.softwaretestingboard.com/customer/account/'
 
 
-@pytest.mark.skip
-@pytest.mark.xfail
+# @pytest.mark.skip
+# @pytest.mark.xfail
 def test_my_wish(driver):
     page = LoginPage(driver)
     page.sign_in(page)
     page = MainPage(driver)
     page.open()
-    sleep(30)
+    # sleep(30)
     page.dropdown().click()
     page.link_my_wish().click()
     assert page.current_url == 'https://magento.softwaretestingboard.com/wishlist/'
 
 
-@pytest.mark.skip
-@pytest.mark.xfail
+# @pytest.mark.skip
+# @pytest.mark.xfail
 def test_sign_out(driver):
     page = LoginPage(driver)
     # page.open()
