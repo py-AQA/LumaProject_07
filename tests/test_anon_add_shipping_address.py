a0d4656c918fa88900a233fbfe558b88134b71e6
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-11-06 02:21:57 +0300

    [+] anon add shipping address test: unfinished

--- tests/test_anon_add_shipping_address.py
+++ tests/test_anon_add_shipping_address.py
@@ -1,4 +1,7 @@
 from random import choice
+from time import sleep
+
+import pytest
 
 from data.fake_data import FakeData
 from pages.abstract import Page
@@ -9,15 +12,26 @@ from pages.account_edit import AccountEditPage
 from pages.home import HomePage
 from pages.logout import LogoutPage
 from pages.sign_in import SignInPage
+from pages.add_to_cart_mok import ItemDetailsPage
 
 
 class TestNow(FakeData):
+
+    @pytest.mark.xfail
     def test_anon_add_shipping_address_with_select_state(self, driver):
         LogoutPage(driver)
-        # CreateAccountPage(driver).create(first_name, last_name, email, password)
+
+        page = ItemDetailsPage(driver)
+        page.add_to_cart().click()
+        assert page.msg == ItemDetailsPage.SUCCESS
+
         page = AnonShippigAddressAddPage(driver)
+        page.email = self.email
+        page.first_name = self.first_name
+
+        page.last_name = self.last_name
+
         page.company = self.company
-        page.telephone = self.phone_number
 
         page.country = choice(AnonShippigAddressAddPage.WITH_REGIONS)
         page.state = (state := choice(page.state[1:]))
@@ -26,11 +40,13 @@ class TestNow(FakeData):
         page.street_1 = self.secondary_address
         page.street_2 = self.street_address
         page.street_3 = f"{city} {state} {postcode}"
+        page.telephone = self.phone_number
 
-        # page.set_billing.click()
-        page.save().click()
-        assert page.msg == AnonShippigAddressAddPage.SUCCESS
-        assert page.url == AnonShippigAddressAddPage.URL_DONE
+        sleep(5)
+        page.button_next().click()
+        # assert page.msg == AnonShippigAddressAddPage.SUCCESS
+        # assert page.url == AnonShippigAddressAddPage.URL_DONE
+        sleep(20)
 
     def test_anon_add_shipping_address_with_input_state(self, driver):
         pass
