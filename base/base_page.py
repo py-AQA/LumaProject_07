6aa0b969824b9f90ab4530643ea95591f09700cf
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2023-11-05 19:23:28 +0300

    [+] debug messages with exceptions

--- base/base_page.py
+++ base/base_page.py
@@ -1,3 +1,5 @@
+from time import sleep
+from selenium.common import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
@@ -15,11 +17,17 @@ class BasePage:
     def header(self) -> WebElement:
         return self.is_visible(BasePageLocators.HEADER)
 
-    def is_visible(self, locator) -> WebElement:
-        return wait(self.driver, timeout=15).until(EC.visibility_of_element_located(locator))
+    def is_visible(self, locator: (str, str), timeout: int = 15) -> WebElement:
+        try:
+            return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+        except TimeoutException:
+            print(f"Timeout: Element located by {locator} not visible after {timeout}s")
 
-    def is_clickable(self, locator) -> WebElement:
-        return wait(self.driver, timeout=15).until(EC.element_to_be_clickable(locator))
+    def is_clickable(self, locator: tuple[str, str], timeout: int = 15) -> WebElement:
+        try:
+            return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+        except TimeoutException:
+            print(f"Timeout: Element located by {locator} not clickable after {timeout}s")
 
     @property
     def current_url(self):
