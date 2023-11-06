c937bea35d24390739c3bcc473985948c04773fa
danya-f <danya-f@mail.ru>
2023-11-06 15:46:12 +0300

    add : switch buttons to MyAccount class

--- pages/my_account.py
+++ pages/my_account.py
@@ -45,4 +45,28 @@ class MyAccountPage(BasePage):
     def confirm_new_password(self) -> WebElement:
         return self.is_visible(MyAccountPageLocators.CONFIRM_NEW_PASSWORD)
 
+    # ДОБАВИЛ ДАНЯ
+    def my_account_button(self) -> WebElement:
+        return self.is_clickable(MyAccountPageLocators.MY_ACCOUNT_BUTTON)
 
+    def my_orders_button(self) -> WebElement:
+        return self.is_clickable(MyAccountPageLocators.MY_ORDERS_BUTTON)
+
+    def my_downloadable_products_button(self) -> WebElement:
+        return self.is_clickable(MyAccountPageLocators.MY_DOWNLOADABLE_PRODUCTS_BUTTON)
+
+    def my_wish_list_button(self) -> WebElement:
+        return self.is_clickable(MyAccountPageLocators.MY_WISH_LIST_BUTTON)
+
+    def address_book_button(self) -> WebElement:
+        return self.is_clickable(MyAccountPageLocators.ADDRESS_BOOK_BUTTON)
+
+    def account_information_button(self) -> WebElement:
+        return self.is_clickable(MyAccountPageLocators.ACCOUNT_INFORMATION_BUTTON)
+
+    def stored_payment_methods_button(self) -> WebElement:
+        return self.is_clickable(MyAccountPageLocators.STORED_PAYMENT_METHODS_BUTTON)
+
+    def my_product_reviews_button(self) -> WebElement:
+        return self.is_clickable(MyAccountPageLocators.MY_PRODUCT_REVIEWS_BUTTON)
+    # ДОБАВИЛ ДАНЯ КОНЕЦ
