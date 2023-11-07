ec0a4c5fdc81f0a57f06d60af76491a105c1c819
danya-f <danya-f@mail.ru>
2023-11-08 02:00:34 +0300

    add : create two types place order as gues and user

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -51,7 +51,35 @@ class CheckoutPage(BasePage):
     def checkout_button(self):
         return self.is_clickable(CartLocators.CART_PROCEED_TO_CHECKOUT_BUTTON)
 
-    def fill_all_require_field_us_shipping(self,state, email, firstname, lastname, street_1, city, postcode, phone_number):
+    def step_2_next_button(self):
+        return self.is_clickable(CheckoutPageLocators.CHECKOUT_STEP_2_NEXT_BUTTON)
+
+    def place_order_button(self):
+        return self.is_clickable(CheckoutPageLocators.PLACE_ORDER_BUTTON)
+
+    def order_number_guest(self):
+        return self.is_visible(CheckoutPageLocators.ORDER_NUMBER_GUEST)
+
+    def order_number_user(self):
+        return self.is_visible(CheckoutPageLocators.ORDER_NUMBER_AS_USER)
+
+    def email_order(self):
+        return self.is_visible(CheckoutPageLocators.EMAIL_ORDER)
+
+    def select_flat_rate_shipping(self):
+        self.is_clickable(CheckoutPageLocators.SHIPPING_FLAT_RATE).click()
+        self.step_2_next_button().click()
+        self.wait_overlay_closed()
+
+    def place_order(self):
+        self.place_order_button().click()
+        self.wait_overlay_closed()
+
+    def success_place_order_message(self):
+        return self.is_visible(CheckoutPageLocators.SUCCESS_PLACE_ORDER_MESSAGE)
+
+    def fill_all_require_field_as_gues_us_shipping(self, state, email, firstname, lastname, street_1, city, postcode,
+                                                   phone_number):
         self.state_dropdown_field().click()
         self.select_state(state)
         self.email_field().send_keys(email)
@@ -62,7 +90,8 @@ class CheckoutPage(BasePage):
         self.postcode_field().send_keys(postcode)
         self.phone_number_field().send_keys(phone_number)
 
-    def fill_all_require_field_ru_shipping(self, email, firstname, lastname, street_1, city, postcode, phone_number):
+    def fill_all_require_field_as_gues_ru_shipping(self, email, firstname, lastname, street_1, city, postcode,
+                                                   phone_number):
         self.country_dropdown_field().click()
         self.select_russia_country().click()
         self.email_field().send_keys(email)
@@ -73,45 +102,52 @@ class CheckoutPage(BasePage):
         self.postcode_field().send_keys(postcode)
         self.phone_number_field().send_keys(phone_number)
 
-    def checkout_button(self):
-        return self.is_clickable(CartLocators.CART_PROCEED_TO_CHECKOUT_BUTTON)
+    def fill_all_require_field_as_user_ru_shipping(self, street_1, city, postcode, phone_number):
+        self.country_dropdown_field().click()
+        self.select_russia_country().click()
+        self.street_address_1_field().send_keys(street_1)
+        self.city_field().send_keys(city)
+        self.postcode_field().send_keys(postcode)
+        self.phone_number_field().send_keys(phone_number)
 
-    def step_2_next_button(self):
-        return self.is_clickable(GuestShippingAddressPageLocators.CHECKOUT_STEP_2_NEXT_BUTTON)
-
-    def place_order_button(self):
-        return self.is_clickable(GuestShippingAddressPageLocators.PLACE_ORDER_BUTTON)
-
-    def order_number(self):
-        return self.is_visible(GuestShippingAddressPageLocators.ORDER_NUMBER)
-
-    def email_order(self):
-        return self.is_visible(GuestShippingAddressPageLocators.EMAIL_ORDER)
-
-    def select_flat_rate_shipping(self):
-        return self.is_clickable(GuestShippingAddressPageLocators.SHIPPING_FLAT_RATE)
+    def fill_all_require_field_as_user_us_shipping(self, state, street_1, city, postcode,
+                                                   phone_number):
+        self.state_dropdown_field().click()
+        self.select_state(state)
+        self.street_address_1_field().send_keys(street_1)
+        self.city_field().send_keys(city)
+        self.postcode_field().send_keys(postcode)
+        self.phone_number_field().send_keys(phone_number)
 
     # ФУНКЦИЯ ВЕШАЕТ ЗАКАЗ КАК ГОСТЬ И ВОЗВРАЩАЕТ НОМЕР ЗАКАЗА
-    def full_guest_place_order_ru(self, email, firstname, lastname, street_1, city, postcode, phone_number) -> str:
+    def full_guest_place_order_ru_address(self, email, firstname, lastname, street_1, city, postcode, phone_number) -> str:
         self.open()
-        self.checkout_button().click()
-        self.fill_all_require_field_ru_shipping(email, firstname, lastname, street_1, city, postcode, phone_number)
-        self.select_flat_rate_shipping().click()
-        self.step_2_next_button().click()
-        self.wait_overlay_closed()
-        self.place_order_button().click()
-        order_id = self.order_number().text
+        self.fill_all_require_field_as_gues_ru_shipping(email, firstname, lastname, street_1, city, postcode,
+                                                        phone_number)
+        self.select_flat_rate_shipping()
+        self.place_order()
+        order_id = self.order_number_guest().text
         assert self.current_url == self.URL_ORDER_PLACED_SUCCESS
         return order_id
 
-    def full_guest_place_order_us(self, state,email, firstname, lastname, street_1, city, postcode, phone_number) -> str:
+    def full_guest_place_order_us_address(self, state, email, firstname, lastname, street_1, city, postcode,
+                                  phone_number) -> str:
         self.open()
-        self.checkout_button().click()
-        self.fill_all_require_field_us_shipping(state,email, firstname, lastname, street_1, city, postcode, phone_number)
-        self.select_flat_rate_shipping().click()
-        self.step_2_next_button().click()
-        self.wait_overlay_closed()
-        self.place_order_button().click()
-        order_id = self.order_number().text
+        self.fill_all_require_field_as_gues_us_shipping(state, email, firstname, lastname, street_1, city, postcode,
+                                                        phone_number)
+        self.select_flat_rate_shipping()
+        self.place_order()
+        order_id = self.order_number_guest().text
+        assert self.current_url == self.URL_ORDER_PLACED_SUCCESS
+        return order_id
+
+    def full_user_place_order_flat_rate(self, state, street_1, city, postcode,
+                                  phone_number) -> str:
+        self.open()
+        self.fill_all_require_field_as_user_us_shipping(state, street_1, city, postcode,
+                                                        phone_number)
+        self.select_flat_rate_shipping()
+        self.place_order()
+        order_id = self.order_number_user().text
         assert self.current_url == self.URL_ORDER_PLACED_SUCCESS
         return order_id
