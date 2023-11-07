4b4411c3177e3ae256a22173fd8e360d376b8bde
danya-f <danya-f@mail.ru>
2023-11-07 20:58:23 +0300

    add : FakeData use

--- tests/checkout_temporary/test_checkout.py
+++ tests/checkout_temporary/test_checkout.py
@@ -1,22 +1,41 @@
+from data.fake_data import FakeData
 from pages.guest_shipping_address_page import GuestShippingAddressPage
 from pages.main_page import MainPage
 
 
-class TestCheckOutGuest:
-    def test_checkout_bolo_sport_watch(self, driver, random_email, random_first_name, random_last_name,
-                                       random_ru_zip_code, random_street, random_city, random_ru_phone_number):
+class TestCheckOutGuest(FakeData):
+    def test_checkout_bolo_sport_watch_ru_shipping(self, driver):
         page = MainPage(driver)
         page.open()
         page.add_bolo_sport_watch_to_cart()
         checkout_page = GuestShippingAddressPage(driver)
         checkout_page.open()
         checkout_page.checkout_button().click()
-        checkout_page.fill_all_require_field(email=random_email, firstname=random_first_name, lastname=random_last_name,
-                                             street_1=random_street, city=random_city, zip_code=random_ru_zip_code,
-                                             phone_number=random_ru_phone_number)
+        email = self.email
+        checkout_page.fill_all_require_field_ru_shipping(email, self.first_name, self.last_name, self.street_address,
+                                                         self.city, self.postcode, self.phone_number)
         checkout_page.select_flat_rate_shipping().click()
         checkout_page.step_2_next_button().click()
         checkout_page.wait_overlay_closed()
         checkout_page.place_order_button().click()
-        assert checkout_page.email_order().text == random_email, 'неправильно отобразился email после заказа'
-        assert checkout_page.current_url == "https://magento.softwaretestingboard.com/checkout/onepage/success/", 'не удалось сделать гостевой заказ'
+        assert checkout_page.email_order().text == email, 'неправильно отобразился email после заказа'
+        assert checkout_page.current_url == GuestShippingAddressPage.URL_ORDER_PLACED_SUCCESS, 'не удалось сделать гостевой заказ'
+
+    def test_checkout_bolo_sport_watch_us_shipping(self, driver):
+        page = MainPage(driver)
+        page.open()
+        page.add_bolo_sport_watch_to_cart()
+        checkout_page = GuestShippingAddressPage(driver)
+        checkout_page.open()
+        checkout_page.checkout_button().click()
+        email = self.email
+        state = self.state
+        checkout_page.fill_all_require_field_us_shipping(state, email, self.first_name, self.last_name,
+                                                         self.street_address, self.city, self.us_postcode_state(state),
+                                                         self.phone_number)
+        checkout_page.select_flat_rate_shipping().click()
+        checkout_page.step_2_next_button().click()
+        checkout_page.wait_overlay_closed()
+        checkout_page.place_order_button().click()
+        assert checkout_page.email_order().text == email, 'неправильно отобразился email после заказа'
+        assert checkout_page.current_url == GuestShippingAddressPage.URL_ORDER_PLACED_SUCCESS, 'не удалось сделать гостевой заказ'
