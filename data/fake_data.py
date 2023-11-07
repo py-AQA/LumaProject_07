9d802032f25e3f0ba5f72c4e34416296fcaf489a
danya-f <danya-f@mail.ru>
2023-11-07 19:29:22 +0300

    add : us_postcode_state

--- data/fake_data.py
+++ data/fake_data.py
@@ -45,3 +45,19 @@ class FakeData:
     @property
     def state(self):
         return Faker().state()
+
+    # ДАНЯ ДОБАВИЛ
+    def us_postcode_state(self, state):
+        converter = {'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA',
+                     'Colorado': 'CO', 'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL', 'Georgia': 'GA',
+                     'Hawaii': 'HI', 'Idaho': 'ID', 'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS',
+                     'Kentucky': 'KY', 'Louisiana': 'LA', 'Maine': 'ME', 'Maryland': 'MD','Massachusetts': 'MA',
+                     'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS', 'Missouri': 'MO',
+                     'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
+                     'New Mexico': 'NM','New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH',
+                     'Oklahoma': 'OK','Oregon': 'OR', 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC',
+                     'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT',
+                     'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI','Wyoming': 'WY'}
+        state = converter[state]
+        return Faker().postcode_in_state(f'{state}')
+# ДАНЯ ДОБАВИЛ КОНЕЦ
