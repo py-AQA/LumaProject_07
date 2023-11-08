77212f686e90f3d32694e85e325543b4774ca3f7
danya-f <danya-f@mail.ru>
2023-11-08 16:31:02 +0300

    edit : PEP8

--- tests/test_add_to_cart.py
+++ tests/test_add_to_cart.py
@@ -3,19 +3,18 @@ from pages.main_page import MainPage
 
 class TestAddToCart:
 
-    def test_add_driven_backpack(self,driver):
+    def test_add_driven_backpack(self, driver):
         page = MainPage(driver)
         page.add_driven_backpack_from_backpacks_catalog_to_cart()
 
-
-    def test_add_clamber_watch(self,driver):
+    def test_add_clamber_watch(self, driver):
         page = MainPage(driver)
         page.add_clamber_watch_from_watches_catalog_to_cart()
 
-    def test_add_montana_wind_jacket_from_men_jacket_to_cart(self,driver):
+    def test_add_montana_wind_jacket_from_men_jacket_to_cart(self, driver):
         page = MainPage(driver)
         page.add_montana_wind_jacket_from_men_jacket_to_cart()
 
-    def test_add_yoga_kit(self,driver):
-        page = MainPage(driver,'https://magento.softwaretestingboard.com/sprite-yoga-companion-kit.html')
+    def test_add_yoga_kit(self, driver):
+        page = MainPage(driver, 'https://magento.softwaretestingboard.com/sprite-yoga-companion-kit.html')
         page.add_yoga_kit_from_gear_fitness_to_cart()
