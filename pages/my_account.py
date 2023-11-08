d9162e4c3f6fa0aa083907d6712e8ddb1b721964
danya-f <danya-f@mail.ru>
2023-11-08 21:20:33 +0300

    delete : open()

--- pages/my_account.py
+++ pages/my_account.py
@@ -4,8 +4,8 @@ from data.locators import MyAccountPageLocators
 
 
 class MyAccountPage(BasePage):
-    def __init__(self, driver, url=MyAccountPageLocators.URL):
-        super().__init__(driver, url)
+    def __init__(self, driver, url=MyAccountPageLocators.URL, open=True):
+        super().__init__(driver, url, open)
 
     def edit_account(self) -> WebElement:
         return self.is_clickable(MyAccountPageLocators.EDIT_ACCOUNT)
