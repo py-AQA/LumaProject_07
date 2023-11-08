14c1509fd670917fd0e63a0c31679f2b856b9c31
danya-f <danya-f@mail.ru>
2023-11-08 20:58:42 +0300

    delete : open()

--- tests/checkout/test_checkout.py
+++ tests/checkout/test_checkout.py
@@ -12,8 +12,9 @@ class TestCheckOutAsGuest(FakeData):
         page.add_item_from_gear_watches_catalog_to_cart(2)
         checkout_page = CheckoutPage(driver)
         email = self.email
-        checkout_page.fill_all_require_field_as_gues_ru_shipping(email, self.first_name, self.last_name, self.street_address,
-                                                         self.city, self.postcode, self.phone_number)
+        checkout_page.fill_all_require_field_as_gues_ru_shipping(email, self.first_name, self.last_name,
+                                                                 self.street_address,
+                                                                 self.city, self.postcode, self.phone_number)
         checkout_page.select_flat_rate_shipping()
         checkout_page.place_order()
         assert checkout_page.email_order().text == email, 'неправильно отобразился email после заказа'
@@ -28,24 +29,26 @@ class TestCheckOutAsGuest(FakeData):
         email = self.email
         state = self.state
         checkout_page.fill_all_require_field_as_gues_us_shipping(state, email, self.first_name, self.last_name,
-                                                         self.street_address, self.city, self.us_postcode_state(state),
-                                                         self.phone_number)
+                                                                 self.street_address, self.city,
+                                                                 self.us_postcode_state(state),
+                                                                 self.phone_number)
         checkout_page.select_flat_rate_shipping()
         checkout_page.place_order()
         assert checkout_page.email_order().text == email, 'неправильно отобразился email после заказа'
         assert checkout_page.current_url == CheckoutPage.URL_ORDER_PLACED_SUCCESS, 'не удалось сделать гостевой заказ'
+
 
 class TestCheckOutAsUser(FakeData):
 
     def test_buy_watches_us_shipping(self, driver):
         page = CreateAccountPage(driver)
         email = self.email
-        page.create(self.first_name,self.last_name,email,self.password)
-        MainPage(driver).add_item_from_gear_watches_catalog_to_cart(2)
+        page.create(self.first_name, self.last_name, email, self.password)
+        MainPage(driver , open=False).add_item_from_gear_watches_catalog_to_cart(2)
         page = CheckoutPage(driver)
         page.open()
         page.fill_all_require_field_as_user_ru_shipping(self.street_address,
-                                                         self.city, self.postcode, self.phone_number)
+                                                        self.city, self.postcode, self.phone_number)
         page.select_flat_rate_shipping()
         page.place_order()
         assert page.success_place_order_message().text == "Thank you for your purchase!"
